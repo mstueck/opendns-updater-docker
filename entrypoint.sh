bfc3214e738b31c15cb2687d82dbe6680bc23a9d
goatie999 <markley79@gmail.com>
2020-05-10 22:00:44 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 
 set -e
 
+sed -i 's|#OPENDNSUSERNAME|$OpenDNS_Username|g' /etc/ddclient/ddclient.conf && /
+	sed -i 's|#OPENDNSPASSWORD|$OpenDNS_Password|g' /etc/ddclient/ddclient.conf && /
+	sed -i 's|#OPENDNSNETWORKLABEL|$OpenDNS_Net-Label|g' /etc/ddclient/ddclient.conf
+
 exec "$@"
