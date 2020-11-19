9713f8bf3190199f3b980380f03b0411d47debe1
goatie999 <markley79@gmail.com>
2020-11-19 15:50:37 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
-currentPubIP=$(curl -s https://diagnostics.opendns.com/myip)
+currentPubIP=$(curl -s https://diagnostic.opendns.com/myip)
 previousPubIP=$currentPubIP
 
-echo $currentPubIP &>/dev/stdout
+echo $currentPubIP 1>/dev/stdout
 
-curl --user "$OpenDNS_Username:$OpenDNS_Password" "https://@updates.opendns.com/nic/update?hostname=$OpenDNS_Net_Label" &>/dev/stdout
+crontab -l > /tmp/CronJobs
+echo "*/10 * * * * curl --user "$OpenDNS_Username:$OpenDNS_Password" "https://updates.opendns.com/nic/update?hostname=$OpenDNS_Net_Label"" >> /tmp/CronJobs
+crontab /tmp/CronJobs
+
+# curl --user "$OpenDNS_Username:$OpenDNS_Password" "https://updates.opendns.com/nic/update?hostname=$OpenDNS_Net_Label" 1>/dev/stdout
 
 exec "$@"
